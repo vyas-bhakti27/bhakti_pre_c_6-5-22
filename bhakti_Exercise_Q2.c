39a7a5af8d37f6314d11f647ebc3ed99dad035f7
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 14:58:48 +0530

    Create bhakti_Exercise_Q2.c

---
+++ bhakti_Exercise_Q2.c
@@ -0,0 +1,14 @@
+/* Exercise */
+
+//Q2)
+
+#include<stdio.h>
+#define MSSG printf("If you lapse, dont collaps\n");
+main()
+{
+    MSSG; // it will replace the MSSG with print statement
+}
+
+
+/* Output:-
+If you lapse, dont collaps */ 
