589a8fbd6865a23c992ff33e241e57f7c2a33e96
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 11:41:29 +0530

    Update bhakti_elif.c

--- bhakti_elif.c
+++ bhakti_elif.c
@@ -1,9 +1,11 @@
+/* Q1) Program to understand the use of #else directives */
+
 #include<stdio.h>
 #define FLAG 2
 main()
 {
     int a=20,b=4;
-    #if FLAG>=3
+    #if FLAG>=3         // if value of flag is greater than or equal to 3 it will print the #if statement otherwise print the #else statements
         printf("value of flag is greater than or equal to 3\n");
         a=a+b;
         b=a*b;
