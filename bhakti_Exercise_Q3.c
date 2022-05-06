fd0cedc4786fb28c73cec4b408a9d5715edd08d0
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 15:09:39 +0530

    Create bhakti_Exercise_Q3.c

---
+++ bhakti_Exercise_Q3.c
@@ -0,0 +1,14 @@
+/* Exercise */
+
+//Q3)
+#include<stdio.h>
+#define PROD(x,y)  ((x)*(y))
+main()
+{
+    int a=3,b=4,x,y;
+    printf("product of a and b = %d",PROD(a,b)); // it will replace x & y with a & b 
+}
+
+
+/* Output:-
+product of a and b = 12 */ 
