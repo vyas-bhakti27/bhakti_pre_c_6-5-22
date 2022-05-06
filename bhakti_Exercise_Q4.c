455a143edd7d9e95e5c76c4e82686fb671a357d7
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 15:28:49 +0530

    Create bhakti_Exercise_Q4.c

---
+++ bhakti_Exercise_Q4.c
@@ -0,0 +1,16 @@
+/* Excercise */
+Q4)
+#include<stdio.h>
+#define A 50
+#define B A+100
+main()
+{
+    int i,j;
+    i=B/20;
+    j=500-B;
+    printf("i = %d, j = %d\n",i,j);
+    
+}
+
+/* Output:-
+i = 55, j = 550 */
