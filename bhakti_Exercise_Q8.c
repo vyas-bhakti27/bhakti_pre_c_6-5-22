0fd43de746cf34806c674dae623f8baf96f1dbb3
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 16:20:21 +0530

    Create bhakti_Exercise_Q8.c

---
+++ bhakti_Exercise_Q8.c
@@ -0,0 +1,15 @@
+// Exercise
+//Q9)
+
+#include <stdio.h>
+#define CUBE(x)  (x*x*x) // CUBE(x) is macro_name & (x*x*x) is macro_expansion
+main()
+{
+  
+    printf("%d\n",CUBE(1+2));
+     
+}
+
+
+/* Output:-
+7
