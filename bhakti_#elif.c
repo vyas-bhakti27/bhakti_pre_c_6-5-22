aba7c72739eb14fcc3bd5bd69be784ba93b0edb4
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 11:51:39 +0530

    Create bhakti_#elif.c

---
+++ bhakti_#elif.c
@@ -0,0 +1,24 @@
+/*Q2) Program to understand the use of #elif directive */
+
+#include<stdio.h>
+#define FLAG 1 // define value of flag to 1
+main()
+{
+    int a=20,b=4;
+    #if FLAG==0
+        printf("value of flag is zero\n");
+        a++;
+        b++;
+    #elif FLAG==1       
+        printf("value of flag  is 1\n");  // it will decrement the value of a-- & b-- because the value of flag=1
+        a--;
+        b--;
+    #elif FLAG==2       
+        printf("value of flag is 2\n");
+        a=a-3;
+        b=a-b;   
+    #endif
+    printf("a = %d, b = %d\n",a,b);
+    printf("program completed \n");
+        
+}
