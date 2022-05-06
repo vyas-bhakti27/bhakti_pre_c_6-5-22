0acc0a45bae6f15c4c47efbdfd82225c3facc072
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-05-06 15:48:55 +0530

    Create bhakti_Exercise_Q5.c

---
+++ bhakti_Exercise_Q5.c
@@ -0,0 +1,25 @@
+// Exercise 
+
+//Q5)
+
+#include <stdio.h>
+#define NEW_LINE printf("\n");
+#define BLANK_LINES(n)  {int i; for(i=0;i<n;i++) printf("\n");} // for loop which from i=0 to i<3 times
+main() {
+    
+    printf("When you have a chance");
+    NEW_LINE
+    printf("to embrance an opporunity");
+    BLANK_LINES(3)
+    printf("Give it a big hug");
+    NEW_LINE
+
+}
+
+/* Output:-
+
+When you have a chance
+to embrance an opporunity
+
+
+Give it a big hug
